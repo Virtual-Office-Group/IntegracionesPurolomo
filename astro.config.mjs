9fbd3fecc28c4fd1199ce0a59b25b1e6df8579e5
Alexander Rodriguez <programador01.vog@gmail.com>
2026-03-15 04:46:07 -0400

    update

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	redirects: {
+		"/": "/guides/sigero",
+	},
+	integrations: [
+		starlight({
+			title: 'Integraciones Purolomo',
+			defaultLocale: 'es',
+			lastUpdated: true,
+			 credits: false,
+			locales: {
+				es: { label: 'Español', lang: 'es' },
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+				{
+					label: 'Integraciones',
+					items: [
+						
+						{ label: 'Integration Sigero', slug: 'guides/sigero' },
+					],
+				},
+			],
+		}),
+	],
+	site: 'https://Virtual-Office-Group.github.io',
+	base: '',
+});
